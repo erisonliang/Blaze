035915d8d1df45d7df3d5156d8c9f85aab769d83
brunomlopes <brunomlopes@cd8aaf68-8842-4e00-9f37-382fa073693d>
2010-03-24 09:28:16 +0000

    Merge branch 'ironpythonplugins'

---
+++ src/IronPythonPlugins/IronPythonHostPlugin.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using SystemCore.CommonTypes;
+using Configurator;
+using IronPython.Hosting;
+using IronPython.Runtime;
+using Microsoft.Scripting.Hosting;
+using Microsoft.Scripting.Hosting.Providers;
+
+namespace IronPythonPlugins
+{
+    [AutomatorPluginAttribute("IronPythonHostPlugin for plugins")]
+    public class IronPythonHostPlugin : InterpreterPlugin
+    {
+        private ScriptEngine _engine;
+        private Dictionary<string, IList<Command>> _plugins;
+
+        public IronPythonHostPlugin() : base("Host for IronPython plugins")
+        {
+            
+        }
+
+        protected override void SetupCommands()
+        {
+            _engine = Python.CreateEngine();
+            _plugins = new Dictionary<string, IList<Command>>();
+
+            var pythonFiles =
+                Directory
+                    .GetFiles(Path.Combine(CommonInfo.PluginsFolder, "IronPythonPlugins"))
+                    .Where(f => f.ToLowerInvariant().EndsWith("ipy"));
+
+            foreach (var pythonFile in pythonFiles)
+            {
+                try
+                {
+                    ScriptSource script = _engine.CreateScriptSourceFromFile(pythonFile);
+                    CompiledCode code = script.Compile();
+                    ScriptScope scope = _engine.CreateScope();
+                    
+                    scope.SetVariable("IIronPythonCommandPlugin", ClrModule.GetPythonType(typeof(IIronPythonCommandPlugin)));
+                    scope.SetVariable("BaseIronPythonCommandPlugin", ClrModule.GetPythonType(typeof(BaseIronPythonCommandPlugin)));
+                    code.Execute(scope);
+                    
+                    _plugins[pythonFile] = new List<Command>();
+                    
+
+                    var pluginClasses = scope.GetItems()
+                        .Where(kvp => kvp.Value is IronPython.Runtime.Types.PythonType)
+                        .Where(kvp => typeof (IIronPythonCommandPlugin).IsAssignableFrom(((IronPython.Runtime.Types.PythonType)kvp.Value).__clrtype__()))
+                        .Select(kvp => kvp.Key)
+                        .Where(c => c != "BaseIronPythonCommandPlugin" && c != "IIronPythonCommandPlugin");
+                    
+
+                    foreach (var p in pluginClasses)
+                    {
+                        var plugin = (IIronPythonCommandPlugin)_engine.Execute(string.Format("{0}()", p), scope);
+
+                        var command = new IronPythonPluginCommand(plugin);
+
+                        _commands.Add(command);
+                        _plugins[pythonFile].Add(command);
+                    }
+                    
+                }
+                catch(Exception e)
+                {
+                    System.Console.Error.Write(string.Format("Error with file {1}: {0}", e, pythonFile));
+                }
+            }
+        }
+
+        protected override string GetAssembyName()
+        {
+            return "IronPythonHostPlugin";
+        }
+
+        protected override string GetAssemblyVersion()
+        {
+            return "1.0.0.0";
+        }
+    }
+}
---
+++ src/IronPythonPlugins/IronPythonPluginCommand.cs
@@ -0,0 +1,19 @@
+﻿using SystemCore.CommonTypes;
+
+namespace IronPythonPlugins
+{
+    class IronPythonPluginCommand : Command
+    {
+        public IronPythonPluginCommand(IIronPythonCommandPlugin plugin)
+            : base(plugin.Name, "Python script " + plugin.Name)
+        {
+            SetIsOwnerDelegate(plugin.IsOwner);
+            SetNameDelegate(plugin.GetName);
+            SetDescriptionDelegate(plugin.GetDescription);
+            SetIconDelegate(str => Resources.python_clear.ToBitmap());
+            SetAutoCompleteDelegate(plugin.AutoComplete);
+            SetUsageDelegate(str => new CommandUsage(plugin.Name));
+            SetExecuteDelegate((parameters, modifiers) => plugin.Execute(parameters));
+        }
+    }
+}
